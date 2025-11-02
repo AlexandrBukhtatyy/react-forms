4ed93cb4a5f984bf162ad087445a45e0bc754bde
Alexandr Bukhtatyy <buhtatyalexander90@gmail.com>
2025-11-02 13:32:46 +0300

    setup msw for playwright and refactor

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router";
-import TablePage from './pages/TablePage.tsx';
-import FormPage from './pages/FormPage.tsx';
-import Form2Page from './pages/Form2Page.tsx';
+import TablePage from "./pages/TablePage.tsx";
+import FormPage from "./pages/FormPage.tsx";
+import Form2Page from "./pages/Form2Page.tsx";
+
+export async function enableMocking() {
+  console.log("import.meta.env.MOCK_API: ", import.meta.env.VITE_API_MOCK);
+  if (import.meta.env.VITE_API_MOCK !== true) {
+    return;
+  }
+  const { serviceWorker } = await import("./mocks/browser.ts");
+
+  return serviceWorker.start();
+}
 
 const router = createBrowserRouter([
   {
@@ -15,12 +25,14 @@ const router = createBrowserRouter([
       { index: true, element: <FormPage /> },
       { path: "form2", element: <Form2Page /> },
       { path: "table", element: <TablePage /> },
-    ]
+    ],
   },
 ]);
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+enableMocking().then(() => {
+  return createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  );
+});
